c45d31e19f961194fcc3aa6fcfc268da75e005f2
Felipe <felipe.r.m.guterres@gmail.com>
2019-10-24 19:23:28 -0300

    Criação do Padrão MVC
    Foi criado os Models, Views e Controllers

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,13 +1,10 @@
 const express = require('express');
-
+const SessionController = require('./controllers/SessionController')
 const routes = express.Router(); //Router responsável pelas rotas do express
 //req.query é para acessar o query params (para filtros)
 //req.params é para acessar route params (para edição, delete)
 //req.body é para acessar corpo da requisição (para criação, edição)
 
-
-routes.post('/users', (req, res) => {
-    return res.json(req.body);
-});
+routes.post('/sessions', SessionController.store);
 
 module.exports = routes; //exportando as rotas
