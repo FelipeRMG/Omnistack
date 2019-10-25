cae6efc111be1477250a9f510adf5507962a046c
Felipe <felipe.r.m.guterres@gmail.com>
2019-10-24 22:31:32 -0300

    Criação do Spot
    Criação do model de Spots, que são as vagas  (empregos)

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,10 +1,19 @@
 const express = require('express');
-const SessionController = require('./controllers/SessionController')
+const multer = require('multer');
+const uploadConfig = require('./config/upload');
+
+const SessionController = require('./controllers/SessionController');
+const SpotController = require('./controllers/SpotController');
+
 const routes = express.Router(); //Router responsável pelas rotas do express
+const upload = multer(uploadConfig);
+
 //req.query é para acessar o query params (para filtros)
 //req.params é para acessar route params (para edição, delete)
 //req.body é para acessar corpo da requisição (para criação, edição)
 
 routes.post('/sessions', SessionController.store);
+routes.post('/spots', upload.single('thumbnail'), SpotController.store);
+
 
 module.exports = routes; //exportando as rotas
