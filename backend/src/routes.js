4ba7ee83f6798ddbc5768858b623fee246f12eb6
Felipe <felipe.r.m.guterres@gmail.com>
2019-10-24 23:25:06 -0300

    Criação do booking
    Criada as reservas, com o relacionamento  do usuário e do spot

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -4,6 +4,8 @@ const uploadConfig = require('./config/upload');
 
 const SessionController = require('./controllers/SessionController');
 const SpotController = require('./controllers/SpotController');
+const DashboardController = require('./controllers/DashboardController');
+const BookingController = require('./controllers/BookingController');
 
 const routes = express.Router(); //Router responsável pelas rotas do express
 const upload = multer(uploadConfig);
@@ -17,5 +19,8 @@ routes.post('/sessions', SessionController.store);
 routes.get('/spots', SpotController.index);
 routes.post('/spots', upload.single('thumbnail'), SpotController.store);
 
+routes.get('/dashboard', DashboardController.show);
+
+routes.post('/spots/:spot_id/bookings', BookingController.store);
 
 module.exports = routes; //exportando as rotas
