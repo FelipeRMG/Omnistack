c1060df815f8ebf6d558256140ad0a643ac3437d
Felipe <felipe.r.m.guterres@gmail.com>
2019-10-29 02:48:44 -0300

    Finalizado o sistema Mobile
    É possível realizar a busca de spots das empresas. Deve corrigir a imagem que no app não aparece

--- mobile/src/pages/Login.js
+++ mobile/src/pages/Login.js
@@ -1,9 +1,35 @@
-import React from 'react';
-import { View, KeyboardAvoidingView, Platform,  Image, Text, TextInput, TouchableOpacity, StyleSheet} from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, AsyncStorage, KeyboardAvoidingView, Platform,  Image, Text, TextInput, TouchableOpacity, StyleSheet} from 'react-native';
+
+import api from '../services/api';
 
 import logo from '../assets/logo.png';
 
-export default function Login(){
+export default function Login({ navigation }){
+    const [email, setEmail] = useState('');
+    const [techs, setTechs] = useState(''); 
+
+    useEffect(() => {
+        AsyncStorage.getItem('user').then(user =>{
+            if(user){
+                navigation.navigate('List');
+            }
+        })
+    }, []);
+
+    async function handleSubmit() {
+        const response = await api.post('/sessions', {
+            email
+        })
+
+        const { _id } = response.data;
+
+        await AsyncStorage.setItem('user', _id);
+        await AsyncStorage.setItem('techs', techs);
+
+        navigation.navigate('List');
+    }
+
     return (
         <KeyboardAvoidingView enabled={Platform.OS === 'ios'} behavior="padding" style={styles.container}>
             <Image source={logo} />
@@ -17,6 +43,8 @@ export default function Login(){
                     keyboardType="email-address"
                     autoCapitalize="none"
                     autoCorrect={false}
+                    value={email}
+                    onChangeText={setEmail}
                 />
 
                 <Text style={styles.label}>TECNOLOGIAS *</Text>
@@ -26,9 +54,12 @@ export default function Login(){
                     placeholderTextColor="#999"
                     autoCapitalize="words"
                     autoCorrect={false}
+                    value={techs}
+                    onChangeText={setTechs}
+
                 />
 
-                <TouchableOpacity style={styles.button}>
+                <TouchableOpacity onPress={handleSubmit} style={styles.button}>
                     <Text style={styles.buttonText}>Encontrar spots</Text>
                 </TouchableOpacity>
             </View>
